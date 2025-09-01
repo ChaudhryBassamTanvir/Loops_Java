053eef17174a046bd98b25035ff4878c165dca28
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-09-01 12:28:24 +0500

    Updated

--- naturalNumbers.java
+++ naturalNumbers.java
@@ -1,12 +1,16 @@
 public class naturalNumbers {
-  //Print all Natural Numbers  
+  //Print all Natural Numbers sum  
 
   public static void main(String[] args) {
     
  int n= 4 ;
+ int sum = 0;
     for (int i = 1; i <= n; i++) {
-        System.out.print(i + " ");
+ sum +=i;
+        System.out.print(i );
         
     }
+
+    System.out.println("The sum of integers is "+sum);
   }
 }
