393bf02a456bc6fb4a214e01ee4fb58130d852cc
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-09-01 12:34:04 +0500

    Updated

--- naturalNumbers.java
+++ naturalNumbers.java
@@ -1,13 +1,18 @@
+import java.util.Scanner;
+
 public class naturalNumbers {
   //Print all Natural Numbers sum  
 
   public static void main(String[] args) {
-    
- int n= 4 ;
+    Scanner sc = new Scanner(System.in);
+
+
+
+ int n= sc.nextInt() ;
  int sum = 0;
     for (int i = 1; i <= n; i++) {
  sum +=i;
-        System.out.print(i );
+        System.out.print(i  + " " );
         
     }
 
