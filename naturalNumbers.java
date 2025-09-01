421af61b711f08928a51aea3d52eb1d9912ca1c8
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-09-01 12:25:04 +0500

    Updated

---
+++ naturalNumbers.java
@@ -0,0 +1,12 @@
+public class naturalNumbers {
+  //Print all Natural Numbers  
+
+  public static void main(String[] args) {
+    
+ int n= 4 ;
+    for (int i = 1; i <= n; i++) {
+        System.out.print(i + " ");
+        
+    }
+  }
+}
